afe581761c07da13cbf10d6841da59b44a1ed60b
Tyom Semonov <inspiro@gmail.com>
2024-10-22 10:32:16 +0100

    Add ESLint import plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
@@ -7,13 +8,42 @@ export default tseslint.config(
     ignores: ['**/dist'],
   },
   eslint.configs.recommended,
+  importPlugin.flatConfigs.typescript,
   ...tseslint.configs.recommended,
   {
+    plugins: { import: importPlugin },
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            {
+              pattern: '~/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+        },
+      ],
     },
   },
 );
